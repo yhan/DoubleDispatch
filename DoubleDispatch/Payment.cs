f2f99651a0ebac40d720f877c477576864b78f4b
Yi HAN <hanyi@8110@gmail.com>
2018-10-01 00:11:22 +0200

    DoubleDispatch

---
+++ DoubleDispatch/Payment.cs
@@ -0,0 +1,61 @@
+﻿namespace DoubleDispatch
+{
+    public class Client
+    {
+        protected int _capital;
+
+        protected Client()
+        {
+        }
+
+        public int Capital => this._capital;
+
+        public virtual string Name => "Unkown client";
+
+        public virtual void IncrementCapital()
+        {
+        }
+    }
+
+    public class Microsoft : Client
+    {
+        public Microsoft()
+        {
+        }
+
+        public override string Name => "Microsoft";
+
+        public override void IncrementCapital()
+        {
+            this._capital += 10000;
+        }
+    }
+
+    class Payment
+    {
+        public virtual string Pay(Microsoft microsoft)
+        {
+            return $"Pays unknown Microsoft with unknown way";
+        }
+
+        public virtual string Pay(Client client)
+        {
+            return $"Pays unknown client with unknown way";
+        }
+    }
+
+    class AliPay : Payment
+    {
+        public override string Pay(Microsoft microsoft)
+        {
+            microsoft.IncrementCapital();
+            return $"Pays {microsoft.Name} through Alibaba";
+        }
+
+        public override string Pay(Client client)
+        {
+            //client.IncrementCapital(); => for Client type, we don't increment capital
+            return $"Pays {client.Name} through Alibaba";
+        }
+    }
+}
---
+++ DoubleDispatch/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace DoubleDispatch
+{
+    using System;
+
+    using NFluent;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DoubleDispatchFailShould
+    {
+        [Test]
+        public void Repro_fails()
+        {
+            Payment payment = new AliPay();
+            Client microsoft = new Microsoft();
+            var pay = payment.Pay(microsoft);
+
+            Check.That(pay).IsEqualTo($"Pays Microsoft through Alibaba");
+            Check.That(microsoft.Capital).IsEqualTo(10000);
+        }
+
+        [Test]
+        public void Repro()
+        {
+            Asteroid theExplodingAsteroidRef = new ExplodingAsteroid();
+            SpaceShip theApolloSpacecraftRef = new ApolloSpacecraft();
+            var collideWith = theExplodingAsteroidRef.CollideWith(theApolloSpacecraftRef);
+
+            Check.That(collideWith).IsEqualTo("ExplodingAsteroid hit a ApolloSpacecraft");
+        }
+    }
+
+    public class SpaceShip
+    {
+        public virtual string GetShipType()
+        {
+            return "SpaceShip";
+        }
+    }
+
+    public class ApolloSpacecraft : SpaceShip
+    {
+        public override string GetShipType()
+        {
+            return "ApolloSpacecraft";
+        }
+    }
+
+    public class Asteroid
+    {
+        public virtual string CollideWith(SpaceShip ship)
+        {
+            return "Asteroid hit a SpaceShip";
+        }
+        public virtual string CollideWith(ApolloSpacecraft ship)
+        {
+           return "Asteroid hit an ApolloSpacecraft";
+        }
+    }
+
+    public class ExplodingAsteroid : Asteroid
+    {
+        public override string CollideWith(SpaceShip ship)
+        {
+            return $"ExplodingAsteroid hit a {ship.GetShipType()}";
+        }
+
+        public override string CollideWith(ApolloSpacecraft ship)
+        {
+            return $"ExplodingAsteroid hit an {ship.GetShipType()}";
+        }
+    };
+}
